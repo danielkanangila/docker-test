4149de7ca6a53e347f45d44ad34ac77016248878
Daniel Kanangila <daniel.kanangila@gmail.com>
2021-06-04 18:38:28 -0400

    update

--- dockertest/settings.py
+++ dockertest/settings.py
@@ -10,20 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Loading environment variable from .env file
+load_dotenv(find_dotenv())
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b=5fr*293fb-uzmqkj654_8*ga)&cqufxa9+7w+5rinj04dom*'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = []
 
@@ -75,10 +81,13 @@ WSGI_APPLICATION = 'dockertest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
+
+# Setting database URL
+DATABASES['default'] = dj_database_url.config(
+    default=os.environ['DATABASE_URL'])
 
 
 # Password validation
