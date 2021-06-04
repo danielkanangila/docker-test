b5797bfda6789f01100a77204392d3540bca90d5
Daniel Kanangila <daniel.kanangila@gmail.com>
2021-06-04 18:50:27 -0400

    update

--- dockertest/settings.py
+++ dockertest/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Loading environment variable from .env file
 load_dotenv(find_dotenv())
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -32,7 +30,6 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
